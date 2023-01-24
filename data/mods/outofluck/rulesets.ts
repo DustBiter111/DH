17f1e9660c877bab8d9bb158615bdee724ca120c
DustBiter111 <123425379+DustBiter111@users.noreply.github.com>
2023-01-23 21:58:05 -0500

    Update rulesets.ts

--- data/mods/outofluck/rulesets.ts
+++ data/mods/outofluck/rulesets.ts
@@ -1,1 +1,19 @@
-
+export const Formats: {[k: string]: FormatData} = {
+	outofluck: {
+		effectType: 'Rule',
+		name: 'Out of Luck',
+		desc: 'h',
+		onBegin() {
+			onAnyInvulnerabilityPriority: 1,
+		onAnyInvulnerability(target, source, move) {
+			if (move && (source === this.effectData.target || target === this.effectData.target)) return 0;
+		},
+		onAnyAccuracy(accuracy, target, source, move) {
+			if (move && (source === this.effectData.target || target === this.effectData.target)) {
+				return true;
+			}
+			return accuracy;
+		},
+		},
+	},
+};
